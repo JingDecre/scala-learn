fdc3b9382fc7dbd3e8574da70cf44d06496d4b3d
JingDecre <l761213825@163.com>
2019-09-06 00:35:17 +0800

    类测试

---
+++ scala-base/src/com/decre/scala/demo/improvetest/PersonTest.scala
@@ -0,0 +1,15 @@
+package com.decre.scala.demo.improvetest
+
+import com.decre.scala.demo.entity.Person
+
+/**
+  * @author Decre
+  * @date 2019-9-6 0:31
+  * @since 1.0.0
+  *        Descirption:
+  */
+object PersonTest {
+  def main(args: Array[String]): Unit = {
+    val p = new Person("dragon", 33, "SH")
+  }
+}
